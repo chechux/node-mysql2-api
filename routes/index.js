4c84d70ace6e8a0134fb8f1f1aeaaf1b0c465446
Jesús Nieto <jnietoribera@gmail.com>
2023-02-01 13:57:48 +0100

    añadimos rutas

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send("hola");
 });
 
+
+router.get('/employes', function(req, res, next) {
+  res.send("Obteniendo empleados...");
+});
+
+router.post('/employes', function(req, res, next) {
+  res.send("Creando empleados...");
+});
+
+router.put('/employes', function(req, res, next) {
+  res.send("Actualizando empleados...");
+});
+
+router.delete('/employes', function(req, res, next) {
+  res.send("Eliminando empleados");
+});
 module.exports = router;
